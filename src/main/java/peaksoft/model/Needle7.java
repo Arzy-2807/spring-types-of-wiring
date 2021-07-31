ba0be37acc475dda26b9fae8c3a9f46285636f49
Admin <arzygul2807@gmail.com>
2021-07-31 16:14:24 +0600

    task is done

--- src/main/java/peaksoft/model/Needle7.java
+++ src/main/java/peaksoft/model/Needle7.java
@@ -1,9 +1,15 @@
 package peaksoft.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+
 
     @Override
     public String toString() {
+
         return ", смерть Кощея на игле :( " + "";
     }
 }
